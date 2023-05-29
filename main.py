68da271078b941864abbdb80d348cef881942a8c
Tytan <tytan@thetytan.com>
2023-05-29 13:44:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import colorama
 from colorama import Fore, Back, Style
 import subprocess
 import time
+import yaml
 
 colorama.init(autoreset=True)
 
@@ -21,7 +22,16 @@ try:
     subprocess.check_output(['git', 'diff', '--exit-code', script_path])
 
     # No modifications found
-    print(f"{Fore.BLACK + Back.WHITE}Script has not been modified")
+    print(f"{Fore.BLACK + Back.WHITE}The script has not been modified within the Git repository.")
+
+    # Load the YAML file
+    with open('license.yaml') as file:
+        license_data = yaml.safe_load(file)
+
+    # Check if the user has agreed to the licenses
+    if not license_data['agree_to_license']:
+        print(f"{Fore.RED}You must agree to the license agreements before running the script.")
+        exit()
 
     # Perform the loading animation
     print(f"Hello and welcome to my script. Here are your options: (Please wait while the script loads)")
@@ -29,5 +39,5 @@ try:
 
 except subprocess.CalledProcessError:
     # Modifications found
-    print(f"{Fore.RED}The script has been modified. Either you modified it or there was an update.\nPlease ensure the integrity of the script.")
+    print(f"{Fore.RED}The script has been modified within the Git repository. Please ensure the integrity of the script.")
 
