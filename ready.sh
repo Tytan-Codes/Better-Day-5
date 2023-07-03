fd6780cb9c5beb60d010075825cfe76f80e93851
Tytan <tytan@thetytan.com>
2023-07-03 15:47:16 -0700

    Update ready.sh

--- ready.sh
+++ ready.sh
@@ -8,12 +8,12 @@ read choice
 
 if [ "$choice" = "1" ]; then
 echo "Performing installation..."
-git clone https://oauth2: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@github.com/tytan-codes/better-day-5.git
+git clone https://github.com/tytan-codes/better-day-5.git
 # Insert your installation code here
 elif [ "$choice" = "2" ]; then
 echo "Performing update..."
 rm -rf better-day-5
-git clone https://oauth2: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@github.com/tytan-codes/better-day-5.git
+git clone https://github.com/tytan-codes/better-day-5.git
 cd better-day-5
 python3 main.py
 # Insert your update code here
