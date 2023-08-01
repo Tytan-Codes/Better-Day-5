4c7fba8730a388588e1cd9353986c0720f04032b
Saul Salazar <115506411+iSaulX@users.noreply.github.com>
2023-07-31 19:06:20 -0600

    Update ready.sh
    Update to make sure the dependecies and packages are installed, if they're installed the program just will skip, its better make sure the packages are installed and the user won't have problems deploying it.

--- ready.sh
+++ ready.sh
@@ -8,7 +8,10 @@ read choice
 
 if [ "$choice" = "1" ]; then
 echo "Performing installation..."
+apt-get install git -y && apt-get install python3 -y 
 git clone https://github.com/tytan-codes/better-day-5.git
+cd better-day-5
+pip install -r requirements.txt && python3 main.py
 # Insert your installation code here
 elif [ "$choice" = "2" ]; then
 echo "Performing update..."
